888d35da2d8e75157dd132573b87d4c489cc51f0
kabhi2004 <abhishekkashyap2501@gmail.com>
2026-05-03 23:32:42 +0530

    Start the Project

---
+++ Project_3/src/App.jsx
@@ -0,0 +1,18 @@
+import Button from './Components/Button'
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <Button> Hello EveryOne</Button>
+     
+    </>
+  )
+}
+
+export default App
